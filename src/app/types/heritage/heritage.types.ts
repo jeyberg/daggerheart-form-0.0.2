d3c5f7adf42eb0263a3f33bae42dc4b0c1015431
jeyberg <and.joh@hotmail.de>
2026-05-15 22:59:19 +0200

    Change type of name property in Ancestry and Community type to string

--- src/app/types/heritage/heritage.types.ts
+++ src/app/types/heritage/heritage.types.ts
@@ -3,14 +3,14 @@ import { Feature } from "../feature.types";
 export const ancestries = ["Clank", "Drakona", "Dwarf", "Elf", "Faerie", "Faun", "Firbolg", "Fungril", "Galapa", "Giant", "Goblin", "Halfling", "Human", "Inferis", "Katari", "Orc", "Ribbet", "Simiah"] as const;
 export type AncestryName = typeof ancestries[number];
 export interface Ancestry {
-    name: AncestryName;
+    name: string;
     features: Feature[]
 }
 
 export const communities = ["Highborne", "Loreborne", "Orderborne", "Ridgeborne", "Seaborne", "Slyborne", "Underborne", "Wanderborne", "Wildborne"] as const;
 export type CommunityName = typeof communities[number];
 export interface Community {
-    name: CommunityName;
+    name: string;
     feature: Feature;
     featureId: number;
 }
