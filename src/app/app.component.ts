b32e96a9248ba6e29c1522d762f6e62e1e20f15d
jeyberg <and.joh@hotmail.de>
2026-06-22 07:26:49 +0200

    Navigate to character creation if there is no character data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ChangeDetectionStrategy, inject, OnInit } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { CharacterFormComponent } from './character-creation/character-form.component';
+import { LocalStorageService } from './local-storage.service';
 
 @Component({
     selector: 'app-root',
-    imports: [CharacterFormComponent],
+    imports: [RouterOutlet],
     templateUrl: './app.component.html',
     changeDetection: ChangeDetectionStrategy.Eager,
     styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'daggerheart-form-0.0.2';
+
+  storage = inject(LocalStorageService);
+  router = inject(Router);
+
+  characterData = this.storage.getCharacterData();
+
+  public ngOnInit(): void {
+    if (!this.characterData()) {
+      this.router.navigate(['/character-creation']);
+    }
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { CharacterFormComponent } from './character-creation/character-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'character-creation',
+        component: CharacterFormComponent
+    }
+];
